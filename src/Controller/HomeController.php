13797343316196f3fc82bd0c903968e29c10640e
richardcrete <crete.richard.ritchi@gmail.com>
2024-11-08 20:56:35 +0100

    Create homepage & redirections

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    public function root(Request $request): RedirectResponse
+    {
+        return $this->redirectToRoute('home', [
+            '_locale' => $request->getPreferredLanguage($this->getParameter('app_locales_array')),
+        ], 301);
+    }
+
+    #[Route('/', name: 'home', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', []);
+    }
+}
