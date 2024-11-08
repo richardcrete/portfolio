fd9435b93e9279f99dc4d61940e20be5cf3c9685
richardcrete <crete.richard.ritchi@gmail.com>
2024-11-08 20:02:20 +0100

    Install Encore

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
