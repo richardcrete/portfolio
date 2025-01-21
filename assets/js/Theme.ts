48f4466f5c907bf3df45e783b600516f251bd4ac
richardcrete <crete.richard.ritchi@gmail.com>
2025-01-21 03:50:38 +0100

    Save theme in cookies

--- assets/js/Theme.ts
+++ assets/js/Theme.ts
@@ -1,3 +1,5 @@
+import * as Cookies from 'es-cookie';
+
 enum ThemeEnum {
     light = "light",
     dark = "dark"
@@ -22,9 +24,19 @@ export default class Theme {
         } else {
             this.theme = ThemeEnum.light;
         }
+
+        if (Cookies.get("theme") === ThemeEnum.light) {
+            this.setCookie("theme", ThemeEnum.dark);
+        } else {
+            this.setCookie("theme", ThemeEnum.light);
+        }
     }
 
     public static toggleButton(): void {
         this.themeSwitcher?.classList.toggle("isLight");
+    }
+
+    public static setCookie(name: string, value: string): void {
+        Cookies.set(name, value, { expires: 30 })
     }
 }
