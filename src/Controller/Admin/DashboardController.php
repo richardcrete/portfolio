2baa45cc540735967364f373b017baaac2882e94
richardcrete <crete.richard.ritchi@gmail.com>
2026-03-03 11:36:24 +0100

    [#10] Generate relative URLs in EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -30,7 +30,7 @@ class DashboardController extends AbstractDashboardController
 
     public function configureDashboard(): Dashboard
     {
-        return Dashboard::new()->setTitle('Admin');
+        return Dashboard::new()->setTitle('Admin')->generateRelativeUrls();
     }
 
     public function configureCrud(): Crud
